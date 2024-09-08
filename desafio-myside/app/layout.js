4a9127230dc4e83b04d9b11503f875e4b2577904
Vinicius Rodrigues <Vinicius.rodrigues5823@gmail.com>
2024-09-08 15:24:48 -0300

    Redux and redux Persist were added and implemented for cart context

--- desafio-myside/app/layout.js
+++ desafio-myside/app/layout.js
@@ -1,13 +1,18 @@
-import StyledComponentsRegistry from '@/app/lib/registry'
-import GlobalStyleProvider from './Components/GlobalStyles/GlobalStyleProvider'
- 
+import StyledComponentsRegistry from '@/app/lib/registry';
+import GlobalStyleProvider from './Components/GlobalStyles/GlobalStyleProvider';
+import ReduxProvider from '@/app/redux/ReduxProvider';
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <body>
-      <GlobalStyleProvider />
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <GlobalStyleProvider />
+        <ReduxProvider>
+          <StyledComponentsRegistry>
+            {children}
+          </StyledComponentsRegistry>
+        </ReduxProvider>
       </body>
     </html>
-  )
+  );
 }
